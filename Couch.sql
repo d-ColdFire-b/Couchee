1ffd2f3c6534cf8212070d63ac78ca2b2a02ade0
d-ColdFire-b <62937452+d-ColdFire-b@users.noreply.github.com>
2020-12-21 01:11:25 +0400

    Added error screen

--- Couch.sql
+++ Couch.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `couch` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `couch`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
 -- Host: localhost    Database: couch
@@ -28,7 +30,7 @@ CREATE TABLE `cart` (
   `propid` int NOT NULL,
   `price` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +39,7 @@ CREATE TABLE `cart` (
 
 LOCK TABLES `cart` WRITE;
 /*!40000 ALTER TABLE `cart` DISABLE KEYS */;
-INSERT INTO `cart` VALUES (1,1,1,100),(2,1,2,10),(3,2,6,200),(4,2,5,15),(5,3,7,1000),(6,3,8,2000);
+INSERT INTO `cart` VALUES (1,1,1,100),(2,1,2,10),(3,2,6,200),(4,2,5,15),(5,3,7,1000),(6,3,8,2000),(7,4,9,2100),(8,4,3,129);
 /*!40000 ALTER TABLE `cart` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,7 +55,7 @@ CREATE TABLE `client` (
   `name` varchar(20) NOT NULL,
   `discount` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,7 +64,7 @@ CREATE TABLE `client` (
 
 LOCK TABLES `client` WRITE;
 /*!40000 ALTER TABLE `client` DISABLE KEYS */;
-INSERT INTO `client` VALUES (1,'OOO Kurp',0),(2,'Bernst',0),(3,'Lapuco',5),(4,'Karakuto',12),(5,'Moster inc',2),(6,'Grandy s',0),(7,'Shevik inc.',90);
+INSERT INTO `client` VALUES (1,'OOO Kurp',0),(2,'Bernst',0),(3,'Lapuco',5),(4,'Karakuto',12),(5,'Moster inc',2),(6,'Grandy s',0),(7,'Shevik inc.',90),(8,'MotorCompany',20),(9,'Creative',52);
 /*!40000 ALTER TABLE `client` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -74,9 +76,9 @@ DROP TABLE IF EXISTS `logins`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `logins` (
-  `Login` varchar(45) NOT NULL,
-  `Password` varchar(45) NOT NULL,
-  `Role` varchar(45) NOT NULL
+  `login` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `role` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -86,7 +88,7 @@ CREATE TABLE `logins` (
 
 LOCK TABLES `logins` WRITE;
 /*!40000 ALTER TABLE `logins` DISABLE KEYS */;
-INSERT INTO `logins` VALUES ('admin','admin',''),('user','user','');
+INSERT INTO `logins` VALUES ('bugalter','buga',1),('worker','work',2);
 /*!40000 ALTER TABLE `logins` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -101,7 +103,7 @@ CREATE TABLE `logs` (
   `id` int NOT NULL AUTO_INCREMENT,
   `textlogtype` varchar(250) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -110,6 +112,7 @@ CREATE TABLE `logs` (
 
 LOCK TABLES `logs` WRITE;
 /*!40000 ALTER TABLE `logs` DISABLE KEYS */;
+INSERT INTO `logs` VALUES (1,'New master have been added - Tommy Vircety'),(2,'New master have been added - Niko Bellicat 20-12-2020 20:02:30'),(3,'New type have been added - boat  At  20-12-2020 20:23:26'),(4,'New prop have been added - Speed 21  At  20-12-2020 20:23:48'),(5,'New client have been added - MotorCompany  At  20-12-2020 20:24:05'),(6,'Client with id:5Get waybill on  20-12-2020 20:31:39'),(7,'New item have been added in cart of waybill - 4Item ID:9  At  20-12-2020 20:31:58'),(8,'New item have been added in cart of waybill - 4Item ID:3  At  20-12-2020 20:32:27'),(9,'Requested info about products maded by master with id1 At20-12-2020 20:38:26'),(10,'Details of the waybill : 1 Have been requested, at  20-12-2020 22:48:08'),(11,'Details of the waybill : 1 Have been requested, at  20-12-2020 22:53:10'),(12,'Details of the waybill : 1 Have been requested, at  20-12-2020 23:03:12'),(13,'Details of the waybill : 3 Have been requested, at  20-12-2020 23:06:10'),(14,'Details of the waybill : 2 Have been requested, at  20-12-2020 23:35:33'),(15,'Details of the waybill : 3 Have been requested, at  20-12-2020 23:35:40'),(16,'Details of the waybill : 3 Have been requested, at  20-12-2020 23:39:28'),(17,'Details of the waybill : 4 Have been requested, at  20-12-2020 23:40:52'),(18,'Details of the waybill : 4 Have been requested, at  20-12-2020 23:41:43'),(19,'Details of the waybill : 4 Have been requested, at  20-12-2020 23:42:33'),(20,'Details of the waybill : 2 Have been requested, at  20-12-2020 23:42:35'),(21,'Details of the waybill : 3 Have been requested, at  20-12-2020 23:42:37'),(22,'Details of the waybill : 1 Have been requested, at  20-12-2020 23:42:38'),(23,'Details of the waybill : 3 Have been requested, at  20-12-2020 23:42:41'),(24,'Details of the waybill : 2 Have been requested, at  20-12-2020 23:45:34'),(25,'Details of the waybill : 2 Have been requested, at  20-12-2020 23:46:11'),(26,'Details of the waybill : 1 Have been requested, at  20-12-2020 23:46:12'),(27,'Details of the waybill : 3 Have been requested, at  20-12-2020 23:46:13'),(28,'Details of the waybill : 4 Have been requested, at  20-12-2020 23:46:14'),(29,'Details of the waybill : 5 Have been requested, at  20-12-2020 23:46:15'),(30,'Details of the waybill : 1 Have been requested, at  20-12-2020 23:46:16'),(31,'Details of the waybill : 4 Have been requested, at  20-12-2020 23:46:17'),(32,'Details of the waybill : 3 Have been requested, at  20-12-2020 23:46:20'),(33,'Requested info about products maded by master with id: 1   At  20-12-2020 23:47:27'),(34,'Requested info about products maded by master with id: 1   At  20-12-2020 23:50:56'),(35,'Requested info about products maded by master with id: 13   At  20-12-2020 23:50:57'),(36,'Requested info about products maded by master with id: 23   At  20-12-2020 23:50:58'),(37,'Requested info about products maded by master with id: 3   At  20-12-2020 23:50:59'),(38,'Requested info about products maded by master with id: 2   At  20-12-2020 23:51:02'),(39,'Requested info about products maded by master with id: 3   At  20-12-2020 23:51:03'),(40,'Requested info about products maded by master with id: 4   At  20-12-2020 23:51:04'),(41,'Requested info about products maded by master with id: 1   At  20-12-2020 23:51:07'),(42,'Requested info about products maded by master with id: 2   At  20-12-2020 23:51:10'),(43,'Requested info about products maded by master with id: 2   At  20-12-2020 23:51:11'),(44,'Requested info about products maded by master with id: 2   At  20-12-2020 23:51:19'),(45,'Requested info about products maded by master with id: 4   At  20-12-2020 23:51:26'),(46,'Requested info about products maded by master with id: 1   At  20-12-2020 23:51:30'),(47,'Requested info about products maded by master with id: 2   At  20-12-2020 23:51:31'),(48,'Requested info about products maded by master with id: 3   At  20-12-2020 23:51:33'),(49,'Requested info about products maded by master with id: 1   At  20-12-2020 23:52:33'),(50,'Requested info about products maded by master with id: 3   At  20-12-2020 23:52:34'),(51,'Requested info about all props of type with id: 3   At 20-12-2020 23:53:24'),(52,'Requested info about all props of type with id: 1   At 20-12-2020 23:53:26'),(53,'Requested info about all props of type with id: 2   At 20-12-2020 23:53:28'),(54,'Requested info about all props of type with id: 3   At 20-12-2020 23:53:29'),(55,'Requested info about all props of type with id: 2   At 20-12-2020 23:53:30'),(56,'Requested info about purchases by client with id: 3   At  20-12-2020 23:56:37'),(57,'Requested info about purchases by client with id: 4   At  20-12-2020 23:56:40'),(58,'Requested info about purchases by client with id: 6   At  20-12-2020 23:56:42'),(59,'Requested info about purchases by client with id: 2   At  20-12-2020 23:57:37'),(60,'Requested info about purchases by client with id: 3   At  20-12-2020 23:57:38'),(61,'Requested info about purchases by client with id: 1   At  20-12-2020 23:57:40'),(62,'Requested info about purchases by client with id: 2   At  20-12-2020 23:57:41'),(63,'Requested info about products maded by master with id: 1   At  20-12-2020 23:57:42'),(64,'Requested info about products maded by master with id: 2   At  20-12-2020 23:57:43'),(65,'Requested info about products maded by master with id: 3   At  20-12-2020 23:57:43'),(66,'Requested info about products maded by master with id: 1   At  20-12-2020 23:57:44'),(67,'Requested info about products maded by master with id: 4   At  20-12-2020 23:57:46'),(68,'Requested info about products maded by master with id: 5   At  20-12-2020 23:57:47'),(69,'Requested info about products maded by master with id: 4   At  20-12-2020 23:57:48'),(70,'New client have been added - Creative  At  20-12-2020 23:58:25'),(71,'Details of the waybill : 1 Have been requested, at  20-12-2020 23:59:23'),(72,'Details of the waybill : 2 Have been requested, at  20-12-2020 23:59:34'),(73,'Details of the waybill : 3 Have been requested, at  20-12-2020 23:59:36'),(74,'Details of the waybill : 1 Have been requested, at  20-12-2020 23:59:37'),(75,'Details of the waybill : 2 Have been requested, at  20-12-2020 23:59:44'),(76,'Someone loged in, and start his work'),(77,'Someone loged in, and start his work'),(78,'Someone loged in, and start his work'),(79,'Someone loged in, and start his work'),(80,'Someone loged in, and start his work'),(81,'Someone loged in, and start his work at : 21-12-2020 00:55:21');
 /*!40000 ALTER TABLE `logs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -124,7 +127,7 @@ CREATE TABLE `master` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -133,7 +136,7 @@ CREATE TABLE `master` (
 
 LOCK TABLES `master` WRITE;
 /*!40000 ALTER TABLE `master` DISABLE KEYS */;
-INSERT INTO `master` VALUES (1,'Diesel'),(2,'Matt'),(3,'Naruto'),(4,'Karp'),(5,'Krakus'),(6,'Drossel'),(7,'Bunche'),(8,'Guru'),(9,'henry'),(10,'Belagu'),(11,'V'),(12,'Raven'),(13,'Rock'),(14,'Steve Ostin'),(15,'Brian'),(16,'Murka'),(17,'Dobby');
+INSERT INTO `master` VALUES (1,'Diesel'),(2,'Matt'),(3,'Naruto'),(4,'Karp'),(5,'Krakus'),(6,'Drossel'),(7,'Bunche'),(8,'Guru'),(9,'henry'),(10,'Belagu'),(11,'V'),(12,'Raven'),(13,'Rock'),(14,'Steve Ostin'),(15,'Brian'),(16,'Murka'),(17,'Dobby'),(18,'Tommy Vircety'),(19,'Niko Bellic');
 /*!40000 ALTER TABLE `master` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,7 +153,7 @@ CREATE TABLE `prop` (
   `masterid` int NOT NULL,
   `typeid` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -159,8 +162,32 @@ CREATE TABLE `prop` (
 
 LOCK TABLES `prop` WRITE;
 /*!40000 ALTER TABLE `prop` DISABLE KEYS */;
-INSERT INTO `prop` VALUES (1,'chair 12',1,1),(2,'chair 2',2,1),(3,'table gug',1,2),(4,'Chair 231',4,1),(5,'Karkos',5,5),(6,'Murcard',11,4),(7,'Gaming NSX',4,1),(8,'RGB+',4,2);
+INSERT INTO `prop` VALUES (1,'chair 12',1,1),(2,'chair 2',2,1),(3,'table gug',1,2),(4,'Chair 231',4,1),(5,'Karkos',5,5),(6,'Murcard',11,4),(7,'Gaming NSX',4,1),(8,'RGB+',4,2),(9,'Speed 21',10,7);
 /*!40000 ALTER TABLE `prop` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'ROLE_BUHGALTER'),(2,'ROLE_WORKER');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -174,7 +201,7 @@ CREATE TABLE `type` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -183,7 +210,7 @@ CREATE TABLE `type` (
 
 LOCK TABLES `type` WRITE;
 /*!40000 ALTER TABLE `type` DISABLE KEYS */;
-INSERT INTO `type` VALUES (1,'chair'),(2,'table'),(3,'couch'),(4,'coffin'),(5,'shelf');
+INSERT INTO `type` VALUES (1,'chair'),(2,'table'),(3,'couch'),(4,'coffin'),(5,'shelf'),(7,'boat');
 /*!40000 ALTER TABLE `type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -199,7 +226,7 @@ CREATE TABLE `waybill` (
   `clientid` int NOT NULL,
   `date` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -208,7 +235,7 @@ CREATE TABLE `waybill` (
 
 LOCK TABLES `waybill` WRITE;
 /*!40000 ALTER TABLE `waybill` DISABLE KEYS */;
-INSERT INTO `waybill` VALUES (1,1,'2003-12-12'),(2,2,'2020-12-01'),(3,7,'2020-12-31');
+INSERT INTO `waybill` VALUES (1,1,'2003-12-12'),(2,2,'2020-12-01'),(3,7,'2020-12-31'),(4,5,'2015-01-30');
 /*!40000 ALTER TABLE `waybill` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -728,4 +755,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-20  1:46:52
+-- Dump completed on 2020-12-21  1:06:16
